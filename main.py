bb4210104830c66268302b9cdadd08ef25765774
ULTRACOLA <davidzero1107@gmail.com>
2021-05-06 11:54:50 +0800

    revert

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == '__mains__':
     print_hi('PyCharm')
+else :
+    print('nice try')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
