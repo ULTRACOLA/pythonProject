e1bbce5a19acdd6396e1fb5687528fb6de7103ca
ULTRACOLA <davidzero1107@gmail.com>
2021-05-06 11:52:14 +0800

    change some codes

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__mains__':
+if __name__ == '__main__':
     print_hi('PyCharm')
-else :
-    print('nice try')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
